d59218110a0c641648932bc1f81aafdad551316d
albert779 <aeliav7@gmail.com>
2025-12-24 18:50:45 +0200

    test

--- obj/Release/net9.0/CarsShop.AssemblyInfo.cs
+++ obj/Release/net9.0/CarsShop.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarsShop")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ac5c4a9725afd973e14edd60102dec141f1dcbf")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarsShop")]
